86c7538910a5b2f6cf35023151b36c5188f8d329
Toba Ojo <tojo@axxess.com>
2023-08-01 01:34:30 +0100

    Refactored change password action method in the user controller

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://192.168.43.254:3000'
 ]
 
 module.exports = allowedOrigins;

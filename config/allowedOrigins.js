764eb793e3035029b5103e81e2d19167754e5a8c
Toba Ojo <tojo@axxess.com>
2023-09-12 22:33:25 +0100

    Added client origin to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://localhost:5500',
     'http://localhost:3000',
     'http://192.168.43.254:3000',
-    'https://treasure-kitchen-server.onrender.com'
+    'https://treasure-kitchen-server.onrender.com',
+    'https://whimsical-taiyaki-aca2db.netlify.app'
 ]
 
 module.exports = allowedOrigins;

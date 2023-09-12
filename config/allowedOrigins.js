efb11a7eb778e1487a06d839a003b1c27f91850a
Toba Ojo <tojo@axxess.com>
2023-09-12 21:25:41 +0100

    Added api url to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:5500',
     'http://localhost:3000',
-    'http://192.168.43.254:3000'
+    'http://192.168.43.254:3000',
+    'https://treasure-kitchen-server.onrender.com'
 ]
 
 module.exports = allowedOrigins;

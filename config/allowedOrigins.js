7752e724bc573a70c9decbf483b6d4888ff56861
Toba Ojo <tojo@axxess.com>
2023-09-12 23:48:02 +0100

    Updated site name

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://192.168.43.254:3000',
     'https://treasure-kitchen-server.onrender.com',
-    'https://whimsical-taiyaki-aca2db.netlify.app'
+    'https://treasure-kitchen.netlify.app'
 ]
 
 module.exports = allowedOrigins;
